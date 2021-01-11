7afbf91e9901a58cc2001619022a8c0e452e67df
Victor <victordrulis@gmail.com>
2021-01-11 19:58:26 -0300

    initial app commit

---
+++ app/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>Ases & Brejas</h1>
+  <base-card>Soon...</base-card>
+</template>
+
+<script>
+
+export default {
+  name: 'Ases&Brejas',
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
